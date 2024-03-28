071b7115b9bb3a41c82a9343acb2288e3610d5d3
Rina <cimas@ya.ru>
2024-03-28 18:59:22 +0300

    Работа завершенна

---
+++ MovieQuiz/Models/QuizQuestion.swift
@@ -0,0 +1,14 @@
+//
+//  QuizQuestion.swift
+//  MovieQuiz
+//
+//  Created by Рина Райф on 19.03.2024.
+//
+
+import Foundation
+
+struct QuizQuestion {
+    let image: String
+    let text: String
+    let correctAnswer: Bool
+}
